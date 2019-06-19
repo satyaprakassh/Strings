3f69d2802655eeec3a4f6280cabf0b6aa1228dd8
satyaprakassh <45558101+satyaprakassh@users.noreply.github.com>
2019-06-19 21:19:34 +0530

    initial

--- StringConcepts.java
+++ StringConcepts.java
@@ -290,3 +290,108 @@ class StringConcepts2
     
     }
 }
+
+
+
+/*
+How to create our own immutale class :
+--------------------------------------
+
+- Once we create an object, we can't perform any change to the excisting object. If we try to
+perform any change then with those cahnges a nwe object will be created. This is known as
+immutability.
+
+- We can create cutomized immutale object.
+
+
+- In the following application we are overriding equals() method,toString() method 
+, so that we can compare objects based on content comparison and whenever we try to 
+print the reference variable of the object, instead of printing the hashcode it will
+print the content.
+*/
+
+public class ImmutableObject
+{
+    private int i;
+    ImmutableObject(int i)
+    {
+        this.i=i;
+    }
+    public boolean equals(Object o)
+    {
+        if(o instanceof ImmutableObject)
+        {
+            ImmutableObject io=(ImmutableObject)o;
+            
+            if(this.i==io.i)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public String toString()
+    {
+        Integer in=new Integer(i);
+        return in.toString();
+    }
+    public ImmutableObject modify(int i)
+    {
+        if(this.i==i)
+        {
+            return this;
+        }
+        else
+        {
+            return new ImmutableObject(i);
+        }
+    }
+    public static void main(String []args)
+    {
+        ImmutableObject io1=new ImmutableObject(10);
+        ImmutableObject io2=new ImmutableObject(20);
+        ImmutableObject io3=new ImmutableObject(10);
+        
+        ImmutableObject io4=io1.modify(50);
+        ImmutableObject io5=io1.modify(20);
+        
+        System.out.println(io1==io2);
+        System.out.println(io1.equals(io2));
+        System.out.println(io1==io3);
+        System.out.println(io1.equals(io3));
+        System.out.println(io1==io4);
+        System.out.println(io2.equals(io4));
+        System.out.println(io2==io5);
+        System.out.println(io3.equals(io5));
+        System.out.println(io2.equals(io5));
+        
+        
+        
+        System.out.println(io1);
+        System.out.println(io2);
+        System.out.println(io3);
+        System.out.println(io4);
+        /*
+        false
+        false
+        false
+        true
+        false
+        false
+        false
+        false
+        10
+        20
+        10
+        50
+        */
+        
+    }
+}
